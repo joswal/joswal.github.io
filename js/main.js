fb22ee6c14d3b73327eeb5be34966d41c0f4f4d2
Joswal <princejoswal@gmail.com>
2019-09-18 00:12:11 +0100

    completed the redesign of the site to improve responsiveness

--- js/main.js
+++ js/main.js
@@ -4,11 +4,59 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const homeButton = document.querySelector('#homeButton');
+const workButton = document.querySelector('#workButton');
+const contactButton = document.querySelector('#contactButton');
+
+const allBtn = $("#all");
+const frontendBtn = $("#frontEnd");
+const backendBtn = $("#backEnd");
+const fullstackBtn = $("#fullStack");
+const wordpressBtn = $("#wordPress");
+
 
 // Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+homeButton.addEventListener('click', toggleMenu);
+workButton.addEventListener('click', toggleMenu);
+contactButton.addEventListener('click', toggleMenu);
+
+frontendBtn.click(function () {
+  $(".frontend").show();
+  $(".backend").hide();
+  $(".fullstack").hide();
+  $(".wordpress").hide();
+});
+
+backendBtn.click(function () {
+  $(".backend").show();
+  $(".frontend").hide();
+  $(".fullstack").hide();
+  $(".wordpress").hide();
+});
+
+fullstackBtn.click(function () {
+  $(".backend").hide();
+  $(".frontend").hide();
+  $(".fullstack").show();
+  $(".wordpress").hide();
+});
+
+wordpressBtn.click(function () {
+  $(".frontend").hide();
+  $(".backend").hide();
+  $(".fullstack").hide();
+  $(".wordpress").show();
+});
+
+allBtn.click(function () {
+  $(".frontend").show();
+  $(".backend").show();
+  $(".fullstack").show();
+  $(".wordpress").show();
+})
 
 function toggleMenu() {
   if (!showMenu) {
